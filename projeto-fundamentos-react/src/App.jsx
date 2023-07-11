988408fb5e20c2c7701a447ff43253eaae0ce177
João Pedro <joaopedromuniz56@gmail.com>
2023-07-10 21:00:16 -0300

    Criação do CSS da pág e muito mais..
    Criação do sidebar, configurações gerais, novas pastas e imagens colocadas.

--- projeto-fundamentos-react/src/App.jsx
+++ projeto-fundamentos-react/src/App.jsx
@@ -1,15 +1,30 @@
 // JSX = JavaScript + XML(HTML)
 import{ Post } from './Post';
+import { Header } from './components/Header';
+
+import styles from './App.module.css'
+
+import './global.css';
+import { Sidebar } from './components/sidebar';
+
 
 function App(props) {
   return (
     <div>
-      <Post author='João Pedro' 
-      content='Meu primeiro content na linguagem react.'
-      />
-      <Post author="Gabriel Jesus" 
-      content="Ruim de bola, mas serviu de exemplo."
-      />
+      <Header/>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author='João Pedro' 
+            content='Meu primeiro content na linguagem react.'
+          />
+          <Post 
+            author="Gabriel Jesus" 
+            content="Ruim de bola, mas serviu de exemplo."
+          />
+        </main>
+      </div>
     </div>
   )
 }
